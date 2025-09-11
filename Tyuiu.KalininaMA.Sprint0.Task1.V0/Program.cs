5f0d85b21bf57b4aa1b4773a8a02114e9ae9506a
ya_zae <kalininalmaria@gmail.com>
2025-09-11 21:28:08 +0500

    Task1

---
+++ Tyuiu.KalininaMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Маша";
+        int age = 18;
+        Console.WriteLine("Здравствуйте, меня зовут "+name +" мне "+age+" лет" );
+        Console.ReadKey();
+    }
+}
