9b59f770545ecef19493af82cb9b661a60561ed5
ya_zae <kalininalmaria@gmail.com>
2025-09-18 14:55:13 +0500

    Task4

---
+++ Tyuiu.KalininaMA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KalininaMA.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KalininaMA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+ }
+        
