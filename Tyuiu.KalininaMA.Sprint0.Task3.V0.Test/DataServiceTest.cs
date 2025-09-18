f29e50f7a8bb29ae9b0fad57dc568b7e999ae728
ya_zae <kalininalmaria@gmail.com>
2025-09-18 14:15:33 +0500

    Task3

---
+++ Tyuiu.KalininaMA.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,14 @@
+﻿
+using Tyuiu.KalininaMA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KalininaMA.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
---
+++ Tyuiu.KalininaMA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.KalininaMA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KalininaMA.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
